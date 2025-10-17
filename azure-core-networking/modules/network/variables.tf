dede5fa65f5feb79e6b35b101f20febaf2692b72
Mohammed Jeelani <sam123092025@outlook.com>
2025-10-17 17:02:50 +0530

    changes in variablesfile

--- azure-core-networking/modules/network/variables.tf
+++ azure-core-networking/modules/network/variables.tf
@@ -1,5 +1,8 @@
-variable "project_name" {
-  description = "The name of the project, used for resource naming and tagging."
+# NOTE: This file is located in the 'Module' directory.
+# It uses 'resource_group_name' because the pipeline passes this value.
+
+variable "resource_group_name" {
+  description = "The name of the resource group, used for resource naming and tagging."
   type        = string
 }
 
@@ -11,9 +14,11 @@ variable "location" {
 variable "vnet_address_space" {
   description = "The address space (CIDR block) for the Virtual Network (e.g., [\"10.50.0.0/16\"])."
   type        = list(string)
+  default     = ["10.50.0.0/16"] # Adding a default makes the root main.tf cleaner
 }
 
 variable "subnet_address_prefix" {
   description = "The address prefix (CIDR block) for the subnet (e.g., [\"10.50.1.0/24\"])."
   type        = list(string)
+  default     = ["10.50.1.0/24"] # Adding a default makes the root main.tf cleaner
 }

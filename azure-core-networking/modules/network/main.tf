b1a9cccce43fc90f71d6c4fc7fa69656e3390211
Mohammed Jeelani <sam123092025@outlook.com>
2025-10-18 15:44:12 +0530

    hhf

--- azure-core-networking/modules/network/main.tf
+++ azure-core-networking/modules/network/main.tf
@@ -1,4 +1,3 @@
-# This block sets up the Azure provider configuration.
 terraform {
   required_providers {
     azurerm = {
@@ -8,29 +7,19 @@ terraform {
   }
 }
 
-# The Azure provider configuration.
 provider "azurerm" {
   features {}
 }
 
-# -----------------------------------------------------
-# Locals: Define derived values once for reuse
-# -----------------------------------------------------
-
+# Define local variable for project name
 locals {
-  # Concatenate project_prefix and environment to create a base name for all resources
   project_name = "${var.project_prefix}-${var.environment}"
 }
 
-# -----------------------------------------------------
-# Module: Core Network Deployment
-# -----------------------------------------------------
-
+# Call the module
 module "core_network" {
-  # !!! IMPORTANT: Check this line !!!
   source = "./modules/network"
 
-  # All other required variables:
   project_name          = local.project_name
   location              = var.location
   vnet_address_space    = var.vnet_address_space

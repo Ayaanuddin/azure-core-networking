627e5eba88a2a55d2509cb982735a0715760b64f
Mohammed Jeelani <sam123092025@outlook.com>
2025-10-18 15:45:06 +0530

    fhhf

--- azure-core-networking/modules/network/variables.tf
+++ azure-core-networking/modules/network/variables.tf
@@ -1,27 +1,29 @@
-# This file declares the variables that the CLI passes into the Root Module.
-# The variable names here MUST match the names used in your -var flags.
-
-variable "resource_group_name" {
-  description = "The name of the resource group to be created. Used as a unique identifier for resources."
+variable "project_prefix" {
+  description = "Prefix for project resources"
   type        = string
+  default     = "myproject"
+}
+
+variable "environment" {
+  description = "Deployment environment (dev, prod, etc.)"
+  type        = string
+  default     = "dev"
 }
 
 variable "location" {
-  description = "The Azure region where resources will be deployed (e.g., eastus)."
+  description = "Azure location for resources"
   type        = string
+  default     = "eastus"
 }
 
-# This is an optional variable to allow overriding the VNet CIDR block.
-# It uses the default value defined here if no value is provided.
 variable "vnet_address_space" {
-  description = "The address space (CIDR block) for the Virtual Network."
+  description = "Address space for the Virtual Network"
   type        = list(string)
-  default     = ["10.50.0.0/16"]
+  default     = ["10.0.0.0/16"]
 }
 
-# This is an optional variable to allow overriding the Subnet CIDR block.
 variable "subnet_address_prefix" {
-  description = "The address prefix (CIDR block) for the subnet."
+  description = "Address prefix for the subnet"
   type        = list(string)
-  default     = ["10.50.1.0/24"]
+  default     = ["10.0.1.0/24"]
 }

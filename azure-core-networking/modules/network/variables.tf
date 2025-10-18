909e8a7c13296d247fb6d9d85f9edf2818dcd201
Mohammed Jeelani <sam123092025@outlook.com>
2025-10-18 14:53:45 +0530

    sma

--- azure-core-networking/modules/network/variables.tf
+++ azure-core-networking/modules/network/variables.tf
@@ -1,28 +1,27 @@
-# This file MUST be in your main/root project directory.
-# It declares the variables that your pipeline is passing in via the command line (-var flags).
+# This file declares the variables that the CLI passes into the Root Module.
+# The variable names here MUST match the names used in your -var flags.
 
-# 1. Resource Group Name (REQUIRED by your pipeline)
 variable "resource_group_name" {
-  description = "The name of the Resource Group, which will be used as the base name for all resources."
+  description = "The name of the resource group to be created. Used as a unique identifier for resources."
   type        = string
 }
 
-# 2. Azure Location (REQUIRED by your pipeline)
 variable "location" {
-  description = "The Azure region where resources will be deployed (e.g., 'eastus')."
+  description = "The Azure region where resources will be deployed (e.g., eastus)."
   type        = string
 }
 
-# 3. VNet Address Space (For configuration)
+# This is an optional variable to allow overriding the VNet CIDR block.
+# It uses the default value defined here if no value is provided.
 variable "vnet_address_space" {
   description = "The address space (CIDR block) for the Virtual Network."
   type        = list(string)
-  default     = ["10.10.0.0/16"]
+  default     = ["10.50.0.0/16"]
 }
 
-# 4. Subnet Address Prefix (For configuration)
+# This is an optional variable to allow overriding the Subnet CIDR block.
 variable "subnet_address_prefix" {
   description = "The address prefix (CIDR block) for the subnet."
   type        = list(string)
-  default     = ["10.10.1.0/24"]
+  default     = ["10.50.1.0/24"]
 }
